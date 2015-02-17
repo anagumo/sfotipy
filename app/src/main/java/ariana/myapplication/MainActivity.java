e8966a010923156fd20367c443452af2af80cfc6
ariana <arianarsdev@gmail.com>
2015-02-17 12:14:35 -0600

    Activity MyMusic

--- app/src/main/java/ariana/myapplication/MainActivity.java
+++ app/src/main/java/ariana/myapplication/MainActivity.java
@@ -1,12 +1,15 @@
 package ariana.myapplication;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Outline;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -26,8 +29,10 @@ public class MainActivity extends ActionBarActivity {
         
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        return super.onCreateOptionsMenu(menu);
+    public void OnClick(View view){
+
+        Toast.makeText(this, "¡Funciona!", Toast.LENGTH_LONG).show();
+        Intent intent = new Intent(this, MyMusic.class);
+        startActivity(intent);
     }
 }
