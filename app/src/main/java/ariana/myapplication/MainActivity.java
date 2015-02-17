9da3250d3960120f09c53827a21ff391e1e576d8
ariana <arianarsdev@gmail.com>
2015-02-17 11:36:37 -0600

    Floating Action Button

--- app/src/main/java/ariana/myapplication/MainActivity.java
+++ app/src/main/java/ariana/myapplication/MainActivity.java
@@ -1,10 +1,12 @@
 package ariana.myapplication;
 
 import android.app.Activity;
+import android.graphics.Outline;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageButton;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -13,6 +15,15 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        /*
+        Sombra del float button en API 21
+        int size = getResources().getDimensionPixelSize(R.dimen.fab_size);
+        Outline outline = new Outline();
+        outline.setOval(0, 0, size, size);
+        ImageButton imageButton = (ImageButton) findViewById(R.id.float_bottom);
+        imageButton.setOutline(outline);*/
+        
     }
 
     @Override
